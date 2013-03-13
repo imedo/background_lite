766ced133602925a53e28c554bbfb20569edd5b5
Alex Coles <alex@alexbcoles.com>
2013-03-13 13:20:05 +0100

    Move ErrorReporter and Handler classes out of core_ext
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- lib/background_lite.rb
+++ lib/background_lite.rb
@@ -4,10 +4,10 @@ require File.dirname(__FILE__) + '/background_lite/core_ext/class'
 require File.dirname(__FILE__) + '/background_lite/core_ext/numeric'
 require File.dirname(__FILE__) + '/background_lite/core_ext/symbol'
 require File.dirname(__FILE__) + '/background_lite/core_ext/nil_class'
-Dir.glob(File.dirname(__FILE__) + '/background_lite/core_ext/handlers/*.rb').each do |handler|
+Dir.glob(File.dirname(__FILE__) + '/background_lite/handlers/*.rb').each do |handler|
   require handler
 end
-Dir.glob(File.dirname(__FILE__) + '/background_lite/core_ext/error_reporters/*.rb').each do |reporter|
+Dir.glob(File.dirname(__FILE__) + '/background_lite/error_reporters/*.rb').each do |reporter|
   require reporter
 end
 require File.dirname(__FILE__) + '/background_lite/rails_ext/activerecord/base'

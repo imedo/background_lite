ed51c4dccc31c6ed18489464f3233a983660dc64
Alex Coles <alex@alexbcoles.com>
2013-03-13 13:46:09 +0100

    Fix mocha require deprecation warning
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source :rubygems
 
 gem 'rake',         '~> 10.0.3'
 gem 'activerecord', '~> 3.2.12'
-gem 'mocha',        '~> 0.13.3'
+gem 'mocha',        '~> 0.13.3', :require => false
 gem 'rdoc'
 
 gemspec
--- test/abstract_unit.rb
+++ test/abstract_unit.rb
@@ -2,7 +2,8 @@ require 'rubygems'
 require 'active_support'
 require 'active_record'
 require 'test/unit'
-require 'mocha'
 require File.dirname(__FILE__) + '/../lib/background_lite'
 
 RAILS_ENV = 'test'
+
+require 'mocha/setup'
